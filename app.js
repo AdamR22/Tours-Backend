08df30173dd52941ed394895f62675ea5b869832
Adam Rao <raoadam87@gmail.com>
2023-02-04 17:29:52 +0300

    Add create new tour endpoint
    Create, add new tour, endpoint.
    Create function that gets new tour from request, saves it to tours-simple.json,
     and returns newly created tour as response.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
     fs.readFileSync(
         `${__dirname}/dev-data/data/tours-simple.json`
@@ -19,7 +21,29 @@ app.get('/v1/tours', function (req, res) {
     });
 });
 
-const port = 3000;
+app.post('/v1/tours', function (req, res) {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign(
+        { id: newId },
+        req.body
+    );
+
+    tours.push(newTour);
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        function (_) {
+            res.status(201).json({
+                status: 'Success',
+                data: {
+                    tour: newTour
+                }
+            });
+        }
+    );
+});
+
+const port = 5000;
 
 app.listen(port, function () {
     console.log(`App running on port ${port}...`)
