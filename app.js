545e9c8e85f8c13d26187e825458e1abea19bd34
Adam Rao <raoadam87@gmail.com>
2023-02-07 21:17:17 +0300

    Add patch and delete endpoints

--- app.js
+++ app.js
@@ -44,7 +44,36 @@ app.get('/v1/tours/:id', function (req, res) {
     });
 });
 
+app.patch('/v1/tours/:id', function(req, res) {
+    const id = parseInt(req.params.id);
 
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid ID'
+        });
+    }     
+
+    res.status(200).json({
+        status: "Success",
+        message: "Tour updated successfully"
+    });
+});
+
+app.delete('/v1/tours/:id', function(req, res) {
+    const id = parseInt(req.params.id);
+
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid ID'
+        });
+    }     
+
+    res.status(204).json({
+        status: "Success",
+    });
+});
 
 app.post('/v1/tours', function (req, res) {
     const newId = tours[tours.length - 1].id + 1;
