9b386e73e325aeb89406bc1938e8b664371b4c26
Adam Rao <raoadam87@gmail.com>
2023-02-13 21:54:06 +0300

    Refactor code

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,18 +1,5 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-    fs.readFileSync(
-        `${__dirname}/../dev-data/data/tours-simple.json`
-    )
-);
-
-exports.checkReqBodyId = function (req, res, next, value) {
-    if (parseInt(req.params.id) > tours.length) {
-        return res.status(404).json({
-            status: 'Fail',
-            message: 'Invalid ID'
-        });
-    }
+exports.checkReqBodyId = function (req, res, next) {
+    //TODO: Implement middleware
     next();
 }
 
@@ -28,70 +15,37 @@ exports.checkReqBodyOk = function (req, res, next) {
 }
 
 exports.getAllTours = function (req, res) {
-    res.status(200).json({
-        status: "Success",
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        },
+    res.status(500).json({
+        status: "Error",
+        message: "Route is not yet defined"
     });
 }
 
 exports.getTour = function (req, res) {
 
-    let id = parseInt(req.params.id);
-
-    const tour = tours.find(function (tour) {
-        return tour.id == id;
-    });
-
-    if (!tour) {
-        res.status(404).json({
-            status: "Fail",
-            message: "Invalid ID"
-        })
-    }
-
-    res.status(200).json({
-        status: "Success",
-        data: {
-            tour
-        },
+    res.status(500).json({
+        status: "Error",
+        message: "Route is not yet defined"
     });
 }
 
 exports.createTour = function (req, res) {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign(
-        { id: newId },
-        req.body
-    );
-
-    tours.push(newTour);
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        function (_) {
-            res.status(201).json({
-                status: 'Success',
-                data: {
-                    tour: newTour
-                }
-            });
-        }
-    );
+    res.status(500).json({
+        status: "Error",
+        message: "Route is not yet defined"
+    });
 }
 
 exports.updateTour = function (req, res) {
-    res.status(200).json({
-        status: "Success",
-        message: "Tour updated successfully"
+    res.status(500).json({
+        status: "Error",
+        message: "Route is not yet defined"
     });
 }
 
 exports.deleteTour = function (req, res) {
-    res.status(204).json({
-        status: "Success",
+    res.status(500).json({
+        status: "Error",
+        message: "Route is not yet defined"
     });
 }
