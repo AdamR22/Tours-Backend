2e4b13ea0796c73fc0c0ba0e8a5ee6e33c3bb4d1
Adam Rao <raoadam87@gmail.com>
2023-02-13 22:42:02 +0300

    Finish API skeleton

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes')
 
 const express = require('express');
-const morgan = require('morgan');
 
 const app = express();
 
-app.use(morgan('dev'));
-
 app.use(express.json());
+
+if (process.env.DEV_ENV === "development") {
+    const morgan = require('morgan');
+    app.use(morgan('dev'));
+}
 
 app.use(function (req, res, next) {
     req.requestTime = new Date().toISOString();
