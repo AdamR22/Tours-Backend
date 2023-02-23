38a2dfceea43b497f2e24a17f0d373ac4e5ea9bc
Adam Rao <raoadam87@gmail.com>
2023-02-23 15:00:53 +0300

    Add tour schema
    Create tour schema with name, price and rating fields.
    Price and name fields are required fields and should return an error if missing.

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config();
 
 const app = require('./app');
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.LOCAL_DB)
+    .then(function (_) {
+        console.log("Connection to DB successful");
+    });
 
 const port = process.env.PORT || 5000;
 
