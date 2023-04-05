6065ed24067941d8196e5c1827d461c85d8cd413
Adam Rao <raoadam87@gmail.com>
2023-04-05 19:31:04 +0300

    Create custom error object

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
-const tourRouter = require('./routes/tourRoutes');
+const ErrorHandler = require("./utils/errorHandlerFactory");
 
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require("./routes/tourRoutes");
 
-const express = require('express');
+const userRouter = require("./routes/userRoutes");
+
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
 if (process.env.DEV_ENV === "development") {
-    const morgan = require('morgan');
-    app.use(morgan('dev'));
+  const morgan = require("morgan");
+  app.use(morgan("dev"));
 }
 
-app.use('/v1/tours', tourRouter);
-app.use('/v1/users', userRouter);
+app.use("/v1/tours", tourRouter);
+app.use("/v1/users", userRouter);
 
-app.all('*', function (_, _, next) {
-    const error = new Error('Endpoint not defined.');
-    error.status = 'Not found';
-    error.statusCode = 404;
-
-    next(error);
+app.all("*", function (_, _, next) {
+  next(new ErrorHandler("Endpoint not defined", 404));
 });
 
 app.use(function (error, _, res, _) {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || "Error";
 
-    error.statusCode = error.statusCode || 500;
-    error.status = error.status || 'Error';
-
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message,
-    });
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+  });
 });
 
 module.exports = app;
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,101 +1,104 @@
-const mongoose = require('mongoose');
-const slugify = require('slugify');
+const mongoose = require("mongoose");
+const slugify = require("slugify");
 
-const tourSchema = new mongoose.Schema({
+const tourSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "A tour must have a name."],
-        unique: true,
-        trim: true
+      type: String,
+      required: [true, "A tour must have a name."],
+      unique: true,
+      trim: true,
     },
     duration: {
-        type: Number,
-        required: [true, "A tour need to have a duration."]
+      type: Number,
+      required: [true, "A tour need to have a duration."],
     },
     maxGroupSize: {
-        type: Number,
-        required: [true, "A tour need to have the number of people taking it."]
+      type: Number,
+      required: [true, "A tour need to have the number of people taking it."],
     },
     difficulty: {
-        type: String,
-        required: [true, "A tour shoud have a difficulty"],
-        enum: {
-            values: ['easy', 'medium', 'difficult'],
-            message: 'Difficulty is either easy, medium or difficult'
-        }
+      type: String,
+      required: [true, "A tour shoud have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either easy, medium or difficult",
+      },
     },
     ratingsAverage: {
-        type: Number,
-        default: 4.5
+      type: Number,
+      default: 4.5,
     },
     ratingsQuantity: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     price: {
-        type: Number,
-        required: [true, "A tour must have a price."],
+      type: Number,
+      required: [true, "A tour must have a price."],
     },
     priceDiscount: {
-        type: Number,
-        validate: {
-            message: 'Discount price should be below regular price',
-            validator: function (val) {
-                return val < this.price;
-            }
-        }
+      type: Number,
+      validate: {
+        message: "Discount price should be below regular price",
+        validator: function (val) {
+          return val < this.price;
+        },
+      },
     },
     summary: {
-        type: String,
-        trim: true,
-        required: [true, "A tour must have a description."]
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a description."],
     },
     description: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
     },
     imageCover: {
-        type: String,
-        required: [true, "A tour must have a cover image."]
+      type: String,
+      required: [true, "A tour must have a cover image."],
     },
     images: [String],
     createdAt: {
-        type: Date,
-        default: Date.now(),
-        select: false
+      type: Date,
+      default: Date.now(),
+      select: false,
     },
     startDates: [Date],
     slug: String,
     secretTour: {
-        type: Boolean,
-        default: false
-    }
-}, {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual("durationInWeeks").get(function () {
+  return this.duration / 7;
 });
 
-tourSchema.virtual('durationInWeeks').get(function () {
-    return this.duration / 7;
-});
-
-tourSchema.pre('save', function (next) {
-    this.slug = slugify(this.name, { lower: true });
-    next();
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 tourSchema.pre(/^find/, function (next) {
-    this.find({ secretTour: { $ne: true } });
-    next();
+  this.find({ secretTour: { $ne: true } });
+  next();
 });
 
-tourSchema.pre('aggregate', function (next) {
-    this.pipeline().unshift({
-        $match: { secretTour: { $ne: true } }
-    });
-    next();
+tourSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } },
+  });
+  next();
 });
 
-const Tour = mongoose.model('Tour', tourSchema);
+const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
